1b2776ee675b6852a2649c096f40584e7091b827
heiwa4126 <heiwa4126@gmail.com>
2024-11-19 10:47:50 +0900

    update

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,23 @@ import { z } from "zod";
 
 export const app = new Hono();
 
+const View = ({ title = "hello hono!" }: { title?: string }) => {
+	return (
+		<html lang="en">
+			<head>
+				<title>{title}</title>
+			</head>
+			<body>
+				<h1>{title}</h1>
+			</body>
+		</html>
+	);
+};
+
+app.get("/", (c) => {
+	return c.html(<View />);
+});
+
 app.get("/hello", (c) => {
 	return c.text("Hello Hono!");
 });
